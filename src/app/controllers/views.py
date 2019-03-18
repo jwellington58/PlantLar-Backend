8887cc1f7684ec54c03b9355340b0723992af9cf
jwellington58 <wj070996@gmail.com>
2019-03-18 16:10:28 -0300

    Crud de Profile

--- src/app/controllers/views.py
+++ src/app/controllers/views.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from app import app, db
-from app.models.tables import User
+from app.models.tables import User, Profile
 
 # users = [
 #     {
@@ -15,6 +15,7 @@ def register_user():
         userRes = User.query.all()
         for us in userRes:
             user = {
+                'id': us.id,
                 'username': us.username,
                 'password': us.password,
             }
@@ -35,3 +36,33 @@ def register_user():
         db.session.commit()
 
         return jsonify({'user': user}), 201
+
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    if request.method == 'GET':
+        profiles = []
+        profileRes = Profile.query.all()
+        for pf in profileRes:
+            profile = {
+                'id': pf.id,
+                'name': pf.name,
+                'user_id': pf.user_id,
+            }
+            profiles.append(profile)
+       
+        return jsonify({'profiles': profiles})
+    else:
+        profile = {
+            'name': request.json['name'],
+            'user_id': request.json['user_id']
+        }
+        name = profile['name']
+        user_id = profile['user_id']
+        #print('UserName {}'.format(username))
+        p = Profile(name, user_id)
+
+        db.session.add(p)
+        db.session.commit()
+
+        return jsonify({'profile': profile}), 201
