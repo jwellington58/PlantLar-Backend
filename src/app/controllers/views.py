ec5e6623446617a96f5fd7b8ce60c91db54309e7
jwellington58 <wj070996@gmail.com>
2019-03-25 09:45:55 -0300

    Separação em metodos diferentes

--- src/app/controllers/views.py
+++ src/app/controllers/views.py
@@ -2,14 +2,9 @@ from flask import Flask, request, jsonify
 from app import app, db
 from app.models.tables import User, Profile, Post
 
-# users = [
-#     {
-
-#     }
-# ]
-
-@app.route('/register', methods=['GET', 'POST'])
-def register_user():
+@app.route('/register', methods=['GET'])
+#crud de user
+def get_user():
     if request.method == 'GET':
         users = []
         userRes = User.query.all()
@@ -22,24 +17,30 @@ def register_user():
             users.append(user)
        
         return jsonify({'users': users})
-    else:
-        user = {
-            'username': request.json['username'],
-            'password': request.json['password']
-        }
-        username = user['username']
-        password = user['password']
-        #print('UserName {}'.format(username))
-        u = User(username, password)
-
-        db.session.add(u)
-        db.session.commit()
-
-        return jsonify({'user': user}), 201
+    
+        
 
 
-@app.route('/profile', methods=['GET', 'POST'])
-def profile():
+@app.route('/register', methods=['POST'])
+def create_user():
+    user = {
+        'username': request.json['username'],
+        'password': request.json['password']
+    }
+    username = user['username']
+    password = user['password']
+    #print('UserName {}'.format(username))
+    u = User(username, password)
+
+    db.session.add(u)
+    db.session.commit()
+
+    return jsonify({'user': user}), 201
+
+
+#Crud de profile
+@app.route('/profile', methods=['GET'])
+def get_profile():
     if request.method == 'GET':
         profiles = []
         profileRes = Profile.query.all()
@@ -52,24 +53,28 @@ def profile():
             profiles.append(profile)
        
         return jsonify({'profiles': profiles})
-    else:
-        profile = {
-            'name': request.json['name'],
-            'user_id': request.json['user_id']
-        }
-        name = profile['name']
-        user_id = profile['user_id']
-        #print('UserName {}'.format(username))
-        p = Profile(name, user_id)
+    
+        
+@app.route('/profile', methods=['POST'])
+def create_profile():
+    profile = {
+        'name': request.json['name'],
+        'user_id': request.json['user_id']
+    }
+    name = profile['name']
+    user_id = profile['user_id']
+    #print('UserName {}'.format(username))
+    p = Profile(name, user_id)
 
-        db.session.add(p)
-        db.session.commit()
+    db.session.add(p)
+    db.session.commit()
 
-        return jsonify({'profile': profile}), 201
+    return jsonify({'profile': profile}), 201
 
 
-@app.route('/post', methods=['GET', 'POST'])
-def post():
+#Crud de Posts
+@app.route('/post', methods=['GET'])
+def get_post():
     if request.method == 'GET':
         posts = []
         postRes = Post.query.all()
@@ -83,19 +88,24 @@ def post():
             posts.append(posts)
        
         return jsonify({'posts': posts})
-    else:
-        post = {
-            'legend': request.json['legend'],
-            'post_date': request.json['post_date'],
-            'profile_id': request.json['profile_id'],
-        }
-        legend = post['legend']
-        post_date = post['post_date']
-        profile_id = post['profile_id']
-        #print('UserName {}'.format(username))
-        p = Post(legend, post_date, profile_id)
+    
+        
 
-        db.session.add(p)
-        db.session.commit()
 
-        return jsonify({'post': post}), 201
+@app.route('/post', methods=['POST'])
+def create_post():
+    post = {
+        'legend': request.json['legend'],
+        'post_date': request.json['post_date'],
+        'profile_id': request.json['profile_id'],
+    }
+    legend = post['legend']
+    post_date = post['post_date']
+    profile_id = post['profile_id']
+    #print('UserName {}'.format(username))
+    p = Post(legend, post_date, profile_id)
+
+    db.session.add(p)
+    db.session.commit()
+
+    return jsonify({'post': post}), 201
